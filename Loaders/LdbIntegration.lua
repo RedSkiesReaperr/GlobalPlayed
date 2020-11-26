5070906098be98b2f8bb3bd77d272a8e7355cf12
Theo Poulet Souchard <theo.pouletsouchard@ls-vr.com>
2020-11-26 12:09:13 +0100

    Add LDB onclick handler

--- Loaders/LdbIntegration.lua
+++ Loaders/LdbIntegration.lua
@@ -1,7 +1,16 @@
 function LoadLdbIntegration()
 	local ldb = LibStub:GetLibrary('LibDataBroker-1.1')
-	local dataobj = ldb:NewDataObject("GlobalPlayed", { type = "data source", text = "GlobalPlayed" })
-	local f = CreateFrame("frame")
+	local dataobj = ldb:NewDataObject("GlobalPlayed", { 
+		type = "data source",
+		text = "GlobalPlayed",
+		OnClick = function(self, button)
+			GlobalPlayed_Log(self.name)
+			if self.name == "GlobalPlayed" then
+				OnClickDataObj(button)
+			end
+		end
+	})
+	local f = CreateFrame("frame", "GlobalPlayedLDBFrame")
 
 	function dataobj:OnTooltipShow()
 		self:SetMinimumWidth(250)
@@ -9,8 +18,8 @@ function LoadLdbIntegration()
 		self:AddLine("GlobalPlayed")
 		self:AddLine(" ")
 
-		-- self:AddDoubleLine("Left click", "Show UI", 0, 0.7, 0.8, 0, 0.7, 0.8)
-		-- self:AddLine(" ")
+		self:AddDoubleLine("Left click", "Show UI", 0, 0.7, 0.8, 0, 0.7, 0.8)
+		self:AddLine(" ")
 
 		local ordered = GetCharactersOrderedByRealm(Characters)
 		for k, v in pairs(ordered) do
@@ -39,9 +48,11 @@ function LoadLdbIntegration()
 		GameTooltip:Hide()
 	end
 
-	-- function OnClickDataObj(button)
-	-- 	if button == "LeftButton" then
-	-- 		OpenUI()
-	-- 	end
-	-- end
+	function OnClickDataObj(button)
+		if button == "LeftButton" then
+			OpenUI()
+		elseif button == "RightButton" then
+			GlobalPlayed_Log("Alo")
+		end
+	end
 end
